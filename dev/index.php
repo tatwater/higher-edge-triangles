905e079271ff69c771387a715fe87246a65adfed
Teagan Atwater <tatwater@outlook.com>
2015-05-07 00:06:07 -0400

    Clean up code for final version

---
+++ dev/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="author" content="Connecticut College Design Public Practice Spring 2015" />
+    <meta name="keywords" content="" />
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    <link rel="stylesheet" href="styles.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="js/scripts.js"></script>
+    <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <title>Profiles</title>
+  </head>
+  <body>
+    <section class="">
+      <div class="content">
+        Hi.
+      </div>
+    </section>
+  </body>
+</html>
